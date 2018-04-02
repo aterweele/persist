0058a0e9681be19ddbb24d604515e8f81c14bc14
Alex ter Weele <act85@cornell.edu>
2018-04-02 15:52:44 -0500

    Rename and fix race condition.
    "file-backed-cache" was the original idea, but this idea has evolved
    into just persisting data structures.
    
    Wow, agents are cool.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject file-backed-cache "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject persist "0.1.0-SNAPSHOT"
+  :description "Persist Clojure data structures to disk."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
---
+++ src/persist/core.clj
@@ -0,0 +1,56 @@
+(ns persist.core
+  (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]
+            [clojure.pprint :as pp]
+            [hasch.core :as hash])
+  (:import [clojure.lang IDeref]
+           [java.io PushbackReader]
+           [java.lang.ref Cleaner]
+           [java.nio.file Files]
+           [java.nio.file.attribute FileAttribute]))
+
+(defonce store-location
+  (Files/createTempDirectory "clj-immut" (make-array FileAttribute 0)))
+
+(defprotocol IStored
+  (store [v] "Persist Clojure value `v` to disk."))
+
+(defonce ^:private cleaner (Cleaner/create))
+
+;; reference counts
+(defonce ^:private store-agent (agent {}))
+
+(defrecord Persisted [file]
+  IDeref
+  (deref [_] (-> file io/reader PushbackReader. edn/read)))
+(defmethod pp/simple-dispatch Persisted [{:keys [file]}] (pr file))
+
+(defn persist
+  [v]
+  (let [file-name (-> v hash/uuid str)
+        f         (io/file store-location file-name)
+        persisted (->Persisted f)]
+    (send-off store-agent
+      (fn [counts]
+        (if (counts file-name)
+          (update counts file-name inc)
+          (do 
+            (with-open [w (io/writer f)]
+              (binding [*out* w
+                        ;; TODO: figure out why *print-length* is
+                        ;; bound to 100 in my REPL.
+                        *print-length* nil]
+                (pr v)))
+            (assoc counts file-name 1)))))
+    (.register cleaner persisted
+      #(send-off store-agent
+         (fn [counts]
+           (let [count (-> file-name counts dec)]
+             (if (zero? count)
+               (do
+                 (println "GCing" f)
+                 (io/delete-file f)
+                 (dissoc counts file-name))
+               (assoc counts file-name count))))))
+    (await store-agent)
+    persisted))
